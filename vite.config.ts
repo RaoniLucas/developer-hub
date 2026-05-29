ade812fd55fbe5b4b92eeaa988ba71c70ed9a10e
Raoni Lucas <RaoniLucas123@gmail.com>
2026-05-29 15:04:52 -0300

    Corrigindo vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/developer-hub/',
   plugins: [react()],
-  server: {
-    host: true,
-    allowedHosts: true,
-  },
 });
